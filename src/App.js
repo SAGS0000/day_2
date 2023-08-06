7cc0dce9888f3ee69c58fb0f2bde52df7cfea9e6
SAGS0000 <113842812+SAGS0000@users.noreply.github.com>
2023-08-06 08:59:55 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.scss";
+import Pic from "./images/icon-star.svg";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const option = [1, 2, 3, 4, 5];
+    const [selectedOption, setSelectedOption] = useState(null);
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const handleOptionChange = (event) => {
+        setSelectedOption(Number(event.target.value));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setIsSubmitted(!isSubmitted);
+    };
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="content">
+                    {!isSubmitted ? (
+                        <>
+                            <div className="logo">
+                                <img
+                                    src={Pic}
+                                    alt=""
+                                />
+                            </div>
+                            <h1>How did we do?</h1>
+                            <p>
+                                Please let us know how we did with your support
+                                request. All feedback is appreciated to help us
+                                improve our offering!
+                            </p>
+                            <div className="rating">
+                                {option.map((option) => (
+                                    <div
+                                        key={option}
+                                        className="custom-radio"
+                                    >
+                                        <input
+                                            value={option}
+                                            type="radio"
+                                            name="rating"
+                                            onChange={handleOptionChange}
+                                            checked={selectedOption === option}
+                                            id={`rating-${option}`}
+                                        />
+                                        <label htmlFor={`rating-${option}`}>
+                                            <span className="custom-radio-button">
+                                                {option}
+                                            </span>
+                                        </label>
+                                    </div>
+                                ))}
+                            </div>
+                            <button onClick={handleSubmit}>Submit</button>
+                        </>
+                    ) : (
+                        <>
+                            <h1>Thankyou</h1>
+                        </>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
